e7853ffbb33b52349554fbb4175f1b316d97a9a0
maxbond <ceo@rf.ru>
2023-09-16 20:53:38 +0300

    Form created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   {id: 1, description: 'Passports', quantity: 2, packed: false},
-  {id: 2, description: 'Socks', quantity: 12, packed: false},
+  {id: 2, description: 'Socks', quantity: 12, packed: true},
+  {id: 3, description: 'charger', quantity: 1, packed: false},
 ];
 
 export default function App() {
@@ -19,10 +20,24 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({length: 20}, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type='text' placeholder='Item...'></input>
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -31,7 +46,7 @@ function PackingList() {
     <div className='list'>
       <ul className='list'>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -41,7 +56,7 @@ function PackingList() {
 function Item({item}) {
   return (
     <li>
-      <span>
+      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
         {item.description} {item.quantity}
       </span>
       <button>❌</button>
