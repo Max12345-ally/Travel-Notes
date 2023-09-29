d62610aa0604ec34fd3135b37c8a3b60be4e52e4
maxbond <ceo@rf.ru>
2023-09-29 23:20:33 +0300

    Button delete added

--- src/App.js
+++ src/App.js
@@ -19,6 +19,13 @@ export default function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm(
+      'Are yo sure you want to delete all items?'
+    );
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className='app'>
       <Logo />
@@ -27,6 +34,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handelToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
@@ -77,7 +85,7 @@ function Form({onAddItems}) {
   );
 }
 
-function PackingList({items, onDeleteItem, onToggleItem}) {
+function PackingList({items, onDeleteItem, onToggleItem, onClearList}) {
   const [sortBy, setSortBy] = useState('input');
   let sortedItems;
 
@@ -111,6 +119,7 @@ function PackingList({items, onDeleteItem, onToggleItem}) {
           <option value='description'>Sort by description</option>
           <option value='packed'>Sort by packed status</option>
         </select>
+        <button onClick={onClearList}>Clear All</button>
       </div>
     </div>
   );
